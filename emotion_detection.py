7855900ddd42278c9c38281459ca0041896cc1f5
kkisza <kkisza@gmail.com>
2025-04-01 12:24:04 -0400

    step 3 completed

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,27 @@
 import requests 
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers = header)
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    #emotions exrtaction from whole formated json
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    
+    #dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    #format the result
+    formatted_emotions = {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion
+    }
+
+    return formatted_emotions
