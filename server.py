57e539f32cb38c2a717ad50ca86c3202e14abeee
kkisza <kkisza@gmail.com>
2025-04-01 13:53:28 -0400

    all steps done

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" This module provides basic functionality for detesting emotions in the text """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,25 +7,24 @@ app = Flask('Emotion Detector')
 
 @app.route("/emotionDetector")
 def emo_detector():
+    """ detects emotions """
     text_to_analyse = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyse)
-   
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
-    else:
-        formatted_response = (
-            f"For the given statement, the system response is "
-            f"'anger': {response['anger']}, "
-            f"'disgust': {response['disgust']}, "
-            f"'fear': {response['fear']}, "
-            f"'joy': {response['joy']} and "
-            f"'sadness': {response['sadness']}. "
-            f"The dominant emotion is {response['dominant_emotion']}."
-        )
-    return  formatted_response 
-
+    formatted_response = (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
+        f"The dominant emotion is {response['dominant_emotion']}."
+    )
+    return  formatted_response
 @app.route("/")
 def render_index_page():
+    """ renders index.html page """
     return render_template('index.html')
 
 if __name__ == "__main__":
